b48b233cf5a9f8043adc627b8f0ad92d46856a6d
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2016-12-28 13:16:33 +0530

    User login added

--- index.rb
+++ index.rb
@@ -4,10 +4,8 @@ require 'base64'
 def encrypt(data)
 	cipher = OpenSSL::Cipher.new('AES-192-CBC')
 	cipher.encrypt
-	$key = cipher.random_key
-	File.open("key.pem","w").write($key)
-	$iv = cipher.random_iv
-	File.open("iv.pem","w").write($iv)
+	$key = 	File.open("key.pem","r").read
+	$iv = File.open("iv.pem","r").read
 	enc = cipher.update(data) + cipher.final
 	enc = Base64.encode64(enc)
 	afile = File.new("text.txt.enc","w")
@@ -15,9 +13,10 @@ def encrypt(data)
 	afile.close
 end
 
-def decrypt(data)
+def decrypted(data)
 	decipher = OpenSSL::Cipher.new('AES-192-CBC')
 	decipher.decrypt
+	decipher.padding = 0
 	decipher.key = File.open("key.pem","r").read
 	decipher.iv = File.open("iv.pem","r").read
 	dec = decipher.update(data)+ decipher.final
@@ -25,15 +24,48 @@ def decrypt(data)
 end
 
 
-puts "1. Lock"
-puts "2. Unlock"
-act = gets.chomp!.to_i
-if(act == 1)
-	puts "Enter data to be encrypted"
-	data = gets.chomp!
-	encrypt(data)
+	def login(name)
+		uname = File.open("uname.pem","r").read
+		
+		if uname == name
+				puts "1. Lock"
+				puts "2. Unlock"
+				act = gets.chomp!.to_i
+			if(act == 1)
+				puts "Enter data to be encrypted"
+				data = gets.chomp!
+				encrypt(data)
+			else
+			data = File.open("text.txt.enc","r").read
+			data = Base64.decode64(data)
+			decrypted(data)
+			end
+		else 
+			print "Wrong Entry"
+		end
+	end
+
+if File::exists?("uname.pem")
+	print "Username: "
+	username = gets.chomp!
+	c = OpenSSL::Digest::SHA1.new
+	enc = c.digest(username)
+	enc = Base64.encode64(enc)
+	login(enc)
 else
-	data = File.open("text.txt.enc","r").read
-	data = Base64.decode64(data)
-	decrypt(data)
+	puts "Entering Program for First time......"
+	print "Enter Username to Store: "
+	username = gets.chomp!
+	c = OpenSSL::Digest::SHA1.new
+	enc = c.digest(username)
+	enc = Base64.encode64(enc)
+	cipher = OpenSSL::Cipher.new('AES-192-CBC')
+	cipher.encrypt
+	File.open("uname.pem","w").write(enc)
+	$key = cipher.random_key
+	File.open("key.pem","w").write($key)
+	$iv = cipher.random_iv
+	File.open("iv.pem","w").write($iv)
+	puts "Username Added..... Program Exiting..."
 end
+
